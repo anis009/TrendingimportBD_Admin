3df0f90971cd43ee81b02be84b7c7cc7b6ad247e
unknown <anis.cse5.bu@gmail.com>
2024-04-07 06:56:30 +0600

    added users list

--- src/redux/Api/api.tsx
+++ src/redux/Api/api.tsx
@@ -1,9 +1,9 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 // import 'vite/client'
-const ENV = import.meta.env.VITE_ENV
-const API_PROD_URL = import.meta.env.VITE_API_PROD_URL
-const API_DEV_URL = import.meta.env.VITE_API_DEV_URL
-const API_STAGING_URL = import.meta.env.VITE_API_STAGING_URL
+const ENV = import.meta.env.VITE_ENV;
+const API_PROD_URL = import.meta.env.VITE_API_PROD_URL;
+const API_DEV_URL = import.meta.env.VITE_API_DEV_URL;
+const API_STAGING_URL = import.meta.env.VITE_API_STAGING_URL;
 
 export const api = createApi({
   reducerPath: 'api',
@@ -16,15 +16,25 @@ export const api = createApi({
     'quotation',
     'clients',
     'client',
-    'orders'
+    'orders',
   ],
   baseQuery: fetchBaseQuery({
     // baseUrl: 'https://first-class-for-less-admin-back-end.vercel.app/api/v1',
-    baseUrl: ENV === 'PROD'
-      ? API_PROD_URL
-      : ENV === 'STAGING'
+    baseUrl:
+      ENV === 'PROD'
+        ? API_PROD_URL
+        : ENV === 'STAGING'
         ? API_STAGING_URL
         : API_DEV_URL,
+    prepareHeaders: (headers, { getState }: any) => {
+      // Get the token from your auth state
+      const token = getState()?.user?.user?.token;
+      console.log('api~~', getState().user, token);
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
   }),
   endpoints: () => ({}),
 });
---
+++ vite-env.d.ts
@@ -0,0 +1,6 @@
+interface ImportMetaEnv {
+  VITE_ENV: string;
+  VITE_API_PROD_URL: string;
+  VITE_API_DEV_URL: string;
+  VITE_API_STAGING_URL: string;
+}
